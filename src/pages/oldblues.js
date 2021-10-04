f313468c2028ceef2dfbcfa0c89822c751ebd67b
Nathanael Walker-Hale <nathanael.walker.hale@gmail.com>
2021-10-04 11:57:49 +0100

    update link to donate

--- src/pages/oldblues.js
+++ src/pages/oldblues.js
@@ -12,7 +12,7 @@ export default function OldBlues({ data }) {
       <h2>Keeping up to date</h2>
       <p>The easiest way to keep up to date with general news of the club is of course by following us on social media (links can be found at the bottom of the page). For alumni-specific news and events, make sure you're on the mailing list. If your're not, then get in contact with our Old Blues officer so that you can be added.</p>
       <h2>Supporting the club</h2>
-      <p>If you enjoyed your time in the club and hope that it continues to perform at the highest level, topping BUCS and smashing Oxf*rd (GDBO) then there is something you can do to help. We have recently set up a donation page that can be accessed <a href="/">here</a>.</p>
+      <p>If you enjoyed your time in the club and hope that it continues to perform at the highest level, topping BUCS and smashing Oxf*rd (GDBO) then there is something you can do to help. We have recently set up a donation page that can be accessed <a href="https://www.philanthropy.cam.ac.uk/give-to-cambridge/fencing-club">here</a>.</p>
       <h2>Events</h2>
       <p>Besides Varsity, the Old Blues match and dinner is an annual event that continues to run. To hear about it and book, please make sure that you're on the mailing list so you don't miss out. If you're not sure or think you may have missed a communication, don't hesitate in contacting us.</p>
     </Page>
